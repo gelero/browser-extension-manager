d18993ce73a382922295cb981e7dd590a4d53b17
Fernando Romero <fromerocontato2@gmail.com>
2025-11-23 21:21:38 -0300

    Feature(cors-politica de segurança)implementando o uso seguro do cors

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,12 @@ const extensionsData = require('./data.json')
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({
-    origin: '*',
-}));
+const allowedOrigins = [
+    'https://browser-extension-manager-ruddy.vercel.app',
+    'http://localhost:5173'
+];
+
+app.use(cors({ origin: allowedOrigins }));
 app.use(express.json())
 
 let extensions = extensionsData.map((ext, index) => ({
@@ -30,20 +33,20 @@ app.patch('/api/extensions/:id/toggle', (req, res) => {
         extensions[index].isActive = !extensions[index].isActive
         res.json(extensions[index])
     } else {
-        res.status(404).json({ message: 'Extensão não encontrada!'})
+        res.status(404).json({ message: 'Extensão não encontrada!' })
     }
 })
 
 app.delete('/api/extensions/:id', (req, res) => {
     const extensionId = parseInt(req.params.id)
     const initialLength = extensions.length
-     
+
     extensions = extensions.filter(ext => ext.id !== extensionId)
 
     if (extensions.length < initialLength) {
         res.status(200).json({ message: 'Extensão removida com sucesso!', id: extensionId })
     } else {
-        res.status(404).json({message: 'Extensão não encontrada!!'})
+        res.status(404).json({ message: 'Extensão não encontrada!!' })
     }
 })
 
